5d564f61e31a9052ee3c3171ec1cae45da612cc8
Marek Grabowski <markgrabowskyy@gmail.com>
2022-06-12 18:43:32 +0200

    add twitter_file

--- conftest.py
+++ conftest.py
@@ -6,4 +6,5 @@ from twitter_core import Twitter
 @pytest.fixture
 def twitter_app():
     twitter_app = Twitter()
-    return twitter_app
+    yield twitter_app
+    twitter_app.delete_tweet()
--- twitter_core.py
+++ twitter_core.py
@@ -1,14 +1,32 @@
+import os.path
 import re
 
 
 class Twitter(object):
-    def __init__(self):
-        self.tweets = []
+    def __init__(self, backend=None):
+        self.backend = backend
+        self._tweets = []
+        if self.backend and not os.path.exists(self.backend):
+            with open(self.backend, 'w'):
+                pass
+
+    @property
+    def tweets(self):
+        if self.backend and not self._tweets:
+            with open(self.backend) as twitter_file:
+                self._tweets = [line.rstrip('\n') for line in twitter_file]
+        return self._tweets
 
     def single_tweet(self, message):
         if len(message) > 160:
             raise Exception("Message too long")
         self.tweets.append(message)
+        if self.backend:
+            with open(self.backend, 'w') as twitter_file:
+                twitter_file.write('\n'.join(self.tweets))
 
     def find_hashtags(self, message):
         return re.findall("#(\w+)", message)
+
+    def delete_tweet(self):  # remember to upgrade this function
+        print('Delete tweet')
